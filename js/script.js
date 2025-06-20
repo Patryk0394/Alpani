611f252392f873792802a3bc6b3a39fa45757660
Patryk <panpatryk94@o2.pl>
2025-06-20 19:31:04 +0200

    Changed handleNav in script.js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,19 @@ const nav = document.querySelector('.nav')
 const burgerNav = document.querySelector('.nav__mobile__burger-btn')
 const burgerBars = document.querySelectorAll('.nav__mobile__burger-btn-bars')
 const mobileNav = document.querySelector('.nav__mobile__links')
+const navItems = document.querySelectorAll('.nav__mobile__links-item')
 
 const scrollNav = 148
 
 const handleNav = () => {
 	mobileNav.classList.toggle('nav__mobile__links--active')
+	burgerBars.forEach(el => el.classList.toggle('nav__mobile__burger-btn-bars--active'))
+	navItems.forEach(item =>
+		item.addEventListener('click', () => {
+			mobileNav.classList.remove('nav__mobile__links--active')
+			burgerBars.forEach(el => el.classList.remove('nav__mobile__burger-btn-bars--active'))
+		})
+	)
 }
 
 const addedBackgroundNav = () => {
@@ -17,12 +25,7 @@ const addedBackgroundNav = () => {
 	}
 }
 
-const burgerBarsAnimation = () => {
-	burgerBars.forEach(el => el.classList.toggle('nav__mobile__burger-btn-bars--active'))
-}
-
 burgerNav.addEventListener('click', handleNav)
 window.addEventListener('scroll', addedBackgroundNav)
-burgerNav.addEventListener('click', burgerBarsAnimation)
 
-// nav__mobile__burger-btn-bars--active
+// burgerBars.forEach(el => el.classList.toggle('nav__mobile__burger-btn-bars--active'))
